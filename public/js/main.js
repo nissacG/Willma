64006fdcbf83f832da44e59e3907654962db2b87
Gareth Cassin <nissacjg@gmail.com>
2018-03-22 13:01:42 +0000

    add scrollTop and color images

---
+++ public/js/main.js
@@ -0,0 +1,20 @@
+// Collapse Navbar
+var navbarCollapse = function() {
+  if ($(document).scrollTop() > 50) {
+    $(".navbar").addClass("d-none");
+  } else {
+    $(".navbar").removeClass("d-none");
+  }
+};
+// Collapse now if page is not at top
+navbarCollapse();
+// Collapse the navbar when page is scrolled
+$(window).scroll(navbarCollapse);
+// Hide navbar when modals trigger
+
+// $(".portfolio-modal").on("show.bs.modal", function(e) {
+//   $(".navbar").addClass("d-none");
+// });
+// $('.portfolio-modal').on('hidden.bs.modal', function(e) {
+//   $(".navbar").removeClass("d-none");
+// });
